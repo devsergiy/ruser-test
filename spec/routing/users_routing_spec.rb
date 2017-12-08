08e959f360e135008bed074594b310d7aec37e93
Sergey Petrunin <neyasut@gmail.com>
2017-12-08 19:29:53 +0200

    routing spec

---
+++ spec/routing/users_routing_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.describe UsersController, type: :routing do
+  describe "routing" do
+
+    it "routes to #index" do
+      expect(:get => "/api/users").to route_to("users#index")
+    end
+
+
+    it "routes to #create" do
+      expect(:post => "/api/users").to route_to("users#create")
+    end
+  end
+end
