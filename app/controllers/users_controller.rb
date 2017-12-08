db6b8e0e70b824f189578867f3cf919c16c370d6
Sergey Petrunin <neyasut@gmail.com>
2017-12-08 19:18:45 +0200

    fix error messages on user creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render :show, status: :created, location: @user
+      render :show, status: :created
     else
-      render json: { errors:  @user.errors }, status: :unprocessable_entity
+      render json: { errors:  @user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
