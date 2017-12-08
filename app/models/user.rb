92b9ee7a62eeadfda8f00db947c92098a68038f6
Sergey Petrunin <neyasut@gmail.com>
2017-12-08 17:08:49 +0200

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  validates_uniqueness_of :email, :phone_number, :key, :account_key
+  validates_presence_of :email, :phone_number, :password
+
+  validates :email, length: { maximum: 200 }
+  validates :phone_number, length: { maximum: 20 }
+  validates :full_name, length: { maximum: 200 }
+  validates :password, length: { maximum: 100 }
+  validates :key, length: { maximum: 100 }
+  validates :account_key, length: { maximum: 100 }
+  validates :metadata, length: { maximum: 2000 }
+end
